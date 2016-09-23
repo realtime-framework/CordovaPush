f1dcf3e896df3056e1e048101fca8ae59cc7ded1
joao caixinha <joaocaixinha@gmail.com>
2016-09-23 11:59:14 +0100

    Run plugin actions on secondary thread, added hash table for commands actions

--- src/android/OrtcPushPlugin.java
+++ src/android/OrtcPushPlugin.java
@@ -1,9 +1,7 @@
 package co.realtime.plugins.android.cordovapush;
 
-import android.app.Activity;
 import android.app.NotificationManager;
 import android.os.Bundle;
-import android.os.Looper;
 import android.util.Log;
 
 import org.apache.cordova.CallbackContext;
@@ -13,6 +11,8 @@ import org.apache.cordova.CordovaWebView;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+
+import java.util.HashMap;
 
 import ibt.ortc.api.Ortc;
 import ibt.ortc.extensibility.OnConnected;
@@ -40,11 +40,11 @@ public class OrtcPushPlugin extends CordovaPlugin {
     public static final String ACTION_CANCEL_NOTIFICATIONS = "cancelAllLocalNotifications";
     public static final String ACTION_SEND_MESSAGE = "send";
     private OrtcClient client;
-    private CallbackContext callback = null;
     private static CordovaWebView gWebView;
     private static Bundle gCachedExtras = null;
     private static boolean gForeground = false;
     private static CordovaInterface sCordova;
+    private static HashMap commands = new HashMap();
     @Override
     public void initialize(final CordovaInterface cordova, CordovaWebView webView) {
         super.initialize(cordova, webView);
@@ -65,7 +65,9 @@ public class OrtcPushPlugin extends CordovaPlugin {
             client.onConnected = new OnConnected() {
                 @Override
                 public void run(OrtcClient ortcClient) {
-                    callback.success();
+                    CallbackContext call = (CallbackContext)commands.get(ACTION_CONNECT);
+                    if (call != null)
+                        call.success();
                 }
             };
 
@@ -81,14 +83,18 @@ public class OrtcPushPlugin extends CordovaPlugin {
                 @Override
                 public void run(OrtcClient ortcClient, String channel) {
                     Log.i(TAG,"Unsubscribed from:" + channel);
-                    callback.success();
+                    CallbackContext call = (CallbackContext)commands.get(ACTION_UNSUBSCRIBE);
+                    if (call != null)
+                        call.success();
                 }
             };
 
             client.onSubscribed = new OnSubscribed() {
                 @Override
                 public void run(OrtcClient ortcClient, String s) {
-                    callback.success();
+                    CallbackContext call = (CallbackContext)commands.get(ACTION_SUBSCRIBE);
+                    if (call != null)
+                        call.success();
                 }
             };
 
@@ -141,9 +147,8 @@ public class OrtcPushPlugin extends CordovaPlugin {
     @Override
     public boolean execute(String action, JSONArray args, final CallbackContext callbackContext) throws JSONException {
 
-        callback = callbackContext;
         gWebView = this.webView;
-
+        final JSONArray argss = args;
         try {
             if (ACTION_LOG.equals(action)) {
                 Log.i(TAG,args.get(0).toString());
@@ -151,44 +156,75 @@ public class OrtcPushPlugin extends CordovaPlugin {
                 return true;
             }
             else if(ACTION_CONNECT.equals(action)){
-                JSONObject arg_object = args.getJSONObject(0);
-                client.setClusterUrl(arg_object.getString("url"));
-                client.setConnectionMetadata(arg_object.getString("metadata"));
-                client.setGoogleProjectId(arg_object.getString("projectId"));
-                client.setApplicationContext(this.cordova.getActivity().getApplicationContext());
-                client.connect(arg_object.getString("appkey"),arg_object.getString("token"));
-
+                commands.put(ACTION_CONNECT, callbackContext);
+                cordova.getThreadPool().execute(new Runnable() {
+                    @Override
+                    public void run() {
+                        JSONObject arg_object = null;
+                        try {
+                            arg_object = argss.getJSONObject(0);
+                            client.setClusterUrl(arg_object.getString("url"));
+                            client.setConnectionMetadata(arg_object.getString("metadata"));
+                            client.setGoogleProjectId(arg_object.getString("projectId"));
+                            client.setApplicationContext(cordova.getActivity().getApplicationContext());
+                            client.connect(arg_object.getString("appkey"),arg_object.getString("token"));
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                });
                 return true;
             }
             else if(ACTION_DISCONNECT.equals(action)){
                 client.disconnect();
                 return true;
             }
-            else if(ACTION_SUBSCRIBE.equals(action)){
-                JSONObject arg_object = args.getJSONObject(0);
-                String channel = arg_object.getString("channel");
-                client.subscribeWithNotifications(channel, true, new OnMessage() {
+            else if(ACTION_SUBSCRIBE.equals(action)) {
+                commands.put(ACTION_SUBSCRIBE, callbackContext);
+                cordova.getThreadPool().execute(new Runnable() {
                     @Override
-                    public void run(OrtcClient ortcClient, final String channel, final String message) {
-                        cordova.getActivity().runOnUiThread(new Runnable() {
-                            @Override
-                            public void run() {
-                                Log.i(TAG, "Received message:" + message + " from channel: " + channel);
-
-                            }
-                        });
-
+                    public void run() {
+                        JSONObject arg_object = null;
+                        try {
+                            arg_object = argss.getJSONObject(0);
+                            String channel = arg_object.getString("channel");
+                            client.subscribeWithNotifications(channel, true, new OnMessage() {
+                                @Override
+                                public void run(OrtcClient ortcClient, final String channel, final String message) {
+                                    cordova.getActivity().runOnUiThread(new Runnable() {
+                                        @Override
+                                        public void run() {
+                                            Log.i(TAG, "Received message:" + message + " from channel: " + channel);
+                                        }
+                                    });
+                                }
+                            });
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
                     }
                 });
                 return true;
             }
             else if(ACTION_UNSUBSCRIBE.equals(action)){
-                JSONObject arg_object = args.getJSONObject(0);
-                String channel = arg_object.getString("channel");
-                client.unsubscribe(channel);
+                commands.put(ACTION_SUBSCRIBE, callbackContext);
+                cordova.getThreadPool().execute(new Runnable() {
+                    @Override
+                    public void run() {
+                        JSONObject arg_object = null;
+                        try {
+                            arg_object = argss.getJSONObject(0);
+                            String channel = arg_object.getString("channel");
+                            client.unsubscribe(channel);
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                });
                 return true;
             }
             else if(ACTION_CANCEL_NOTIFICATIONS.equals(action)){
+                commands.put(ACTION_SUBSCRIBE, callbackContext);
                 final NotificationManager notificationManager = (NotificationManager) this.cordova.getActivity().getSystemService(this.cordova.getActivity().NOTIFICATION_SERVICE);
                 notificationManager.cancelAll();
                 callbackContext.success();
