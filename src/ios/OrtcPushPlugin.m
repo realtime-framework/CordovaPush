5f6f63976df9155393e7f814b6eed14eac94b866
joao caixinha <joaocaixinha@gmail.com>
2016-11-16 11:33:28 +0000

    getIsConnected iOS correction

--- src/ios/OrtcPushPlugin.m
+++ src/ios/OrtcPushPlugin.m
@@ -9,9 +9,8 @@
 #import "OrtcPushPlugin.h"
 
 @implementation OrtcPushPlugin
-
-- (void)connect:(CDVInvokedUrlCommand*)command
-{
+    
+- (void)connect:(CDVInvokedUrlCommand*)command{
     _connectCommand = [[NSMutableDictionary alloc] init];
     [_connectCommand setObject:command forKey:@"connect"];
     NSDictionary* args = [command.arguments objectAtIndex:0];
@@ -33,9 +32,8 @@
     // Connect
     [_ortc connect:appKey authenticationToken:token];
 }
-
-- (void)disconnect:(CDVInvokedUrlCommand*)command
-{
+    
+- (void)disconnect:(CDVInvokedUrlCommand*)command{
     [self trowException:@"ORTC not connected" forCommand:command code:^(CDVInvokedUrlCommand *cmd) {
         [_connectCommand setObject:cmd forKey:@"disconnect"];
         [_ortc disconnect];
@@ -43,16 +41,19 @@
     
     
 }
-
-- (void)getIsConnected:(CDVInvokedUrlCommand*)command
-{
+    
+- (void)getIsConnected:(CDVInvokedUrlCommand*)command{
+    if(_connectCommand == nil){
+        _connectCommand = [[NSMutableDictionary alloc] init];
+    }
+    
     [_connectCommand setObject:command forKey:@"getIsConnected"];
     CDVPluginResult* pluginResult = nil;
-    pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsInt:[_ortc isConnected]];
+    pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsInt:(_ortc != nil?[_ortc isConnected]:0)];
     [self.commandDelegate sendPluginResult:pluginResult callbackId:[[_connectCommand objectForKey:@"getIsConnected"] callbackId]];
 }
-
-
+    
+    
 - (void)trowException:(NSString*)exception forCommand:(CDVInvokedUrlCommand*)command code:(void (^)(CDVInvokedUrlCommand*))code{
     if (!_ortc) {
         [[NSNotificationCenter defaultCenter] postNotificationName:@"onException" object:nil userInfo:@{@"exception":exception}];
@@ -60,53 +61,45 @@
     }
     code(command);
 }
-
-- (void)onConnected:(OrtcClient *)ortc
-{
+    
+- (void)onConnected:(OrtcClient *)ortc{
     CDVPluginResult* pluginResult = nil;
     pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK];
     [self.commandDelegate sendPluginResult:pluginResult callbackId:[[_connectCommand objectForKey:@"connect"] callbackId]];
 }
-
-- (void)onDisconnected:(OrtcClient *)ortc
-{
+    
+- (void)onDisconnected:(OrtcClient *)ortc{
     CDVPluginResult* pluginResult = nil;
     pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK];
     [self.commandDelegate sendPluginResult:pluginResult callbackId:[[_connectCommand objectForKey:@"disconnect"] callbackId]];
 }
-
-- (void)onException:(OrtcClient *)ortc error:(NSError *)error
-{
+    
+- (void)onException:(OrtcClient *)ortc error:(NSError *)error{
     [[NSNotificationCenter defaultCenter] postNotificationName:@"onException" object:nil userInfo:@{@"exception":error.localizedDescription}];
 }
-
-- (void)onReconnected:(OrtcClient *)ortc
-{
+    
+- (void)onReconnected:(OrtcClient *)ortc{
     
 }
-
-- (void)onReconnecting:(OrtcClient *)ortc
-{
+    
+- (void)onReconnecting:(OrtcClient *)ortc{
     
 }
-
-- (void)onSubscribed:(OrtcClient *)ortc channel:(NSString *)channel
-{
+    
+- (void)onSubscribed:(OrtcClient *)ortc channel:(NSString *)channel{
     CDVPluginResult* pluginResult = nil;
     pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK];
     [self.commandDelegate sendPluginResult:pluginResult callbackId:[[_connectCommand objectForKey:[NSString stringWithFormat:@"sub:%@",channel]] callbackId]];
 }
-
-- (void)onUnsubscribed:(OrtcClient *)ortc channel:(NSString *)channel
-{
+    
+- (void)onUnsubscribed:(OrtcClient *)ortc channel:(NSString *)channel{
     CDVPluginResult* pluginResult = nil;
     pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK];
     [self.commandDelegate sendPluginResult:pluginResult callbackId:[[_connectCommand objectForKey:[NSString stringWithFormat:@"unsub:%@",channel]] callbackId]];
 }
-
-
-- (void)subscribe:(CDVInvokedUrlCommand*)command
-{
+    
+    
+- (void)subscribe:(CDVInvokedUrlCommand*)command{
     [self trowException:@"ORTC not connected" forCommand:command code:^(CDVInvokedUrlCommand *cmd) {
         NSString* channel = [[cmd.arguments objectAtIndex:0] objectForKey:@"channel"];
         [_connectCommand setObject:cmd forKey:[NSString stringWithFormat:@"sub:%@",channel]];
@@ -117,16 +110,15 @@
         }];
     }];
 }
-
-- (void)unsubscribe:(CDVInvokedUrlCommand*)command
-{
+    
+- (void)unsubscribe:(CDVInvokedUrlCommand*)command{
     [self trowException:@"ORTC not connected" forCommand:command code:^(CDVInvokedUrlCommand *cmd) {
         NSString* channel = [[cmd.arguments objectAtIndex:0] objectForKey:@"channel"];
         [_connectCommand setObject:cmd forKey:[NSString stringWithFormat:@"unsub:%@",channel]];
         [_ortc unsubscribe:channel];
     }];
 }
-
+    
 - (void)setApplicationIconBadgeNumber:(CDVInvokedUrlCommand*)command{
     
     NSInteger badge = [[command.arguments objectAtIndex:0] integerValue];
@@ -135,9 +127,8 @@
     pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK];
     [self.commandDelegate sendPluginResult:pluginResult callbackId:[command callbackId]];
 }
-
-- (void)send:(CDVInvokedUrlCommand*)command
-{
+    
+- (void)send:(CDVInvokedUrlCommand*)command{
     [self trowException:@"ORTC not connected" forCommand:command code:^(CDVInvokedUrlCommand *cmd) {
         NSDictionary* args = [cmd.arguments objectAtIndex:0];
         
@@ -147,7 +138,7 @@
         [_ortc send:channel message:channelMsg];
     }];
 }
-
+    
 - (void)cancelAllLocalNotifications:(CDVInvokedUrlCommand*)command{
     [[UIApplication sharedApplication] setApplicationIconBadgeNumber: 0];
     [[UIApplication sharedApplication] cancelAllLocalNotifications];
@@ -155,38 +146,36 @@
     pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK];
     [self.commandDelegate sendPluginResult:pluginResult callbackId:[command callbackId]];
 }
-
-
-- (void)checkForNotifications:(CDVInvokedUrlCommand*)command
-{
+    
+    
+- (void)checkForNotifications:(CDVInvokedUrlCommand*)command{
     [[NSNotificationCenter defaultCenter] postNotificationName:@"checkForNotifications" object:@"OrtcPushPlugin"];
     CDVPluginResult* pluginResult = nil;
     pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK];
     [self.commandDelegate sendPluginResult:pluginResult callbackId:[command callbackId]];
 }
-
+    
 - (void)removeNotifications:(CDVInvokedUrlCommand*)command
-{
-    [[NSNotificationCenter defaultCenter] postNotificationName:@"removeNotifications" object:@"OrtcPushPlugin"];
-    CDVPluginResult* pluginResult = nil;
-    pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK];
-    [self.commandDelegate sendPluginResult:pluginResult callbackId:[command callbackId]];
-}
-
-- (void)log:(CDVInvokedUrlCommand*)command
-{
+    {
+        [[NSNotificationCenter defaultCenter] postNotificationName:@"removeNotifications" object:@"OrtcPushPlugin"];
+        CDVPluginResult* pluginResult = nil;
+        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK];
+        [self.commandDelegate sendPluginResult:pluginResult callbackId:[command callbackId]];
+    }
+    
+- (void)log:(CDVInvokedUrlCommand*)command{
     NSLog(@"OrtcPushPlugin: %@",[command.arguments objectAtIndex:0]);
 }
-
+    
 - (void)enableHeadsUpNotifications:(CDVInvokedUrlCommand*)command{
-
+    
 }
-
+    
 - (void)disableHeadsUpNotifications:(CDVInvokedUrlCommand*)command{
     
 }
-
-@end
+    
+    @end
 
 
 
